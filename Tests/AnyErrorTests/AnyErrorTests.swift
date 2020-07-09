d7823f5ff5d0de08b2425d334b5bd0b70896705b
Tsungyu Yu <ytyubox@gmail.com>
2020-07-09 18:29:46 +0800

    test add NSError, String, noArgument

--- Tests/AnyErrorTests/AnyErrorTests.swift
+++ Tests/AnyErrorTests/AnyErrorTests.swift
@@ -33,6 +33,67 @@ final class AnyErrorTests: XCTestCase {
         XCTAssertEqual(anyError.localizedDescription,
                        "test error description")
     }
+    func testNSErrorConvertibility() {
+        var userinfo:[String:Any] = [:]
+        if #available(OSX 10.13, *) {
+            userinfo = [
+                kCFErrorLocalizedFailureReasonKey! as String: "test reason",
+                NSLocalizedRecoverySuggestionErrorKey:"test suggestion",
+                NSLocalizedRecoveryOptionsErrorKey:["test anchor"],
+                NSLocalizedDescriptionKey: "test error description",
+            ]
+        } else {
+            // Fallback on earlier versions
+        }
+        let error = NSError(
+            domain: "test domain",
+            code: 100,
+            userInfo: userinfo)
+//        error.localizedFailureReason = "test reason"
+        
+        let anyError = AnyError(error)
+        
+        XCTAssertEqual(anyError.failureReason,
+                       "test reason")
+        XCTAssertEqual(anyError.recoverySuggestion,
+                       "test suggestion")
+        XCTAssertEqual(anyError.helpAnchor,
+                       "test anchor")
+        XCTAssertEqual(anyError.errorDescription,
+                       "test error description")
+        XCTAssertEqual(anyError.localizedDescription,
+                       "test error description")
+    }
+    func testAnyErrorWithMessage() {
+        let error = "test message"
+        let anyError = AnyError(message: error)
+        
+        XCTAssertEqual(anyError.failureReason,
+                       nil)
+        XCTAssertEqual(anyError.recoverySuggestion,
+                       nil)
+        XCTAssertEqual(anyError.helpAnchor,
+                       nil)
+        XCTAssertEqual(anyError.errorDescription,
+                       nil)
+        XCTAssertEqual(anyError.localizedDescription,
+                       "test message")
+    }
+    func testInitWithArgument() {
+        let anyError = AnyError()
+        
+        XCTAssertEqual(anyError.failureReason,
+                       nil)
+        XCTAssertEqual(anyError.recoverySuggestion,
+                       nil)
+        XCTAssertEqual(anyError.helpAnchor,
+                       nil)
+        XCTAssertEqual(anyError.errorDescription,
+                       nil)
+        XCTAssertEqual(anyError.localizedDescription,
+                       "未知的錯誤")
+    }
+    
 
     static var allTests = [
         ("testCustomLocalizedError", testCustomLocalizedError),
